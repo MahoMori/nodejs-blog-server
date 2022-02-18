7eabde47201e59b417001d60b1f0bcabdea912af
MahoMori <tr27e1104@gmail.com>
2022-02-17 18:04:01 -0800

    brought back prev code

--- index.js
+++ index.js
@@ -20,24 +20,12 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
-// const whitelist = process.env.WHITELIST_DOMAINS
-//   ? process.env.WHITELIST_DOMAINS.split(",")
-//   : [];
-// const corsOption = {
-//   origin: (origin, callback) => {
-//     if (!origin || whitelist.indexOf(origin) !== -1) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error("Not allowed by CORS"));
-//     }
-//   },
-//   credentials: true,
-// };
-
-const whitelist = [process.env.WHITELIST_DOMAINS];
+const whitelist = process.env.WHITELIST_DOMAINS
+  ? process.env.WHITELIST_DOMAINS.split(",")
+  : [];
 const corsOption = {
   origin: (origin, callback) => {
-    if (whitelist.indexOf(origin) !== -1) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
@@ -45,7 +33,6 @@ const corsOption = {
   },
   credentials: true,
 };
-
 app.use(cors(corsOption));
 app.use(passport.initialize());
 
