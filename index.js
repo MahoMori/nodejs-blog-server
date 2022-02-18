b1284307d010dec6d9e1a06e6fe79ed89e7f667d
MahoMori <tr27e1104@gmail.com>
2022-02-17 22:24:36 -0800

    added header

--- index.js
+++ index.js
@@ -21,6 +21,17 @@ app.use(timeout("80s"));
 app.use(bodyParser.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
+  next();
+});
+
 const whitelist = process.env.WHITELIST_DOMAINS
   ? process.env.WHITELIST_DOMAINS.split(",")
   : [];
